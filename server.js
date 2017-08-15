fe1b95655fce73ed4a875a02477f535d562b0d93
chsrinu <chsreenivas92@gmail.com>
2017-08-15 18:08:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article-one',function(req, res){
     res.sendFile(path.join(__dirname,'ui','article-one.html'));
 });
+app.get('/article-two',function(req,res)
+{
+    res.sendFile(path.join(__dirname,'ui','article-two.html'))
+})
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
