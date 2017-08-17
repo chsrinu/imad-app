e1722276ddfe89a248d8e8c37e25f9a778c2ecca
chsrinu <chsreenivas92@gmail.com>
2017-08-17 16:09:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,7 @@ console.log('Loaded!');
 var marginleft=0;
 $(".img-medium").click(function() {
    marginleft+=1;
+  var outp= "clicked " +marginleft+" times";
+   
+   $('#counter').html(outp);
 });
