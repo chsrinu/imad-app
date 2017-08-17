225ae6b95af7fee208e4292bb2e8768a0e3db65f
chsrinu <chsreenivas92@gmail.com>
2017-08-17 16:21:28 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
 console.log('Loaded!');
 var marginleft=0;
 $(".img-medium").click(function() {
-   marginleft+=1;
+   marginleft+=10;
   var outp= "clicked " +marginleft+" times";
-   
    $('#counter').html(outp);
 });
