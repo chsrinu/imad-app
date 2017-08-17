53d51603a2e326e436d54596e3adcca24f2c2453
chsrinu <chsreenivas92@gmail.com>
2017-08-17 19:06:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,19 @@ console.log('Loaded!');
 var myi=document.getElementById("b");
 var leftmargin=0;
  function anim(){
-     
-     console.log("anim");
+     if($('b').is(':offscreen'))
+     {
+     myi.style.marginLeft=0+'px';
+     console.log(myi.style.marginLeft);
+     }
+     else
+     {
+      console.log("anim");
      leftmargin=leftmargin+10;
      myi.style.marginLeft=leftmargin+'px';
      console.log(myi.style.marginLeft);
+     }
+     
  }
  
  
