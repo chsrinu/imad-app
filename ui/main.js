e16dd6b4efc7e82549f1adf682b20557202a62e9
chsrinu <chsreenivas92@gmail.com>
2017-08-17 17:58:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ console.log('Loaded!');
 var marginleft=0;
 $(".img-medium").click(function() {
    alert("clicked");
- var img1=document.getElementsbyclass("img-medium");
+ var img1=document.getElementsbyclassname("img-medium");
  img1.style.left+=1000+'px';
  
 });
