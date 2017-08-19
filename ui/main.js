53ca3abd9c9b05cf34a9e522aead1803e55ec069
chsrinu <chsreenivas92@gmail.com>
2017-08-19 18:18:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,11 +32,11 @@ request.onreadystatechange = function (){
      if(request.status === 200)
         {
            
-            
+            var temp=request.resposeText
             var clickcount=document.getElementById("clickcount");
             
             console.log("getting the response ");
-            clickcount.innerHTML=request.resposeText;
+            clickcount.innerHTML=temp.toString();
         }
     }
 };
