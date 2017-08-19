7ce3417564609eb49473e57429927e764c227659
chsrinu <chsreenivas92@gmail.com>
2017-08-19 18:05:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,13 +29,13 @@ clicker.onclick=function() {
 request.onreadystatechange = function (){
     if(request.readyState==XMLHttpRequest.DONE)
     {
-     if(request.status == 200)
+     if(request.status === 200)
         {
            
             var temp=request.resposeText;
             var clickcount=document.getElementById("clickcount");
             
-            console.log("getting the response "+temp);
+            console.log("getting the response ");
             clickcount.innerHTML=temp.toString();
         }
     }
