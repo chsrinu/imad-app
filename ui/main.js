61235cf25a62f7d1fe64ff30421bc18589fd0a6d
chsrinu <chsreenivas92@gmail.com>
2017-08-19 18:04:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ var clicker=document.getElementById("abc");
 
 clicker.onclick=function() {
     var request=new XMLHttpRequest();
-request.onReadStateChange = function (){
+request.onreadystatechange = function (){
     if(request.readyState==XMLHttpRequest.DONE)
     {
      if(request.status == 200)
