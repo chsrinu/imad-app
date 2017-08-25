4ed40750f9a441474458976a3f4c4763996727cf
chsrinu <chsreenivas92@gmail.com>
2017-08-25 17:18:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/testdb',function(req,res){
         if(err)
             res.status(500).send(err.toString());
         else
-            res.send(JSON.stringify(results))
+            res.send(JSON.stringify(results.rows))
     })
     
 });
