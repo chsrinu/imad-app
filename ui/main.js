bc08ee281c33a2a578cc058cbece815d6f1aa5fe
chsrinu <chsreenivas92@gmail.com>
2017-09-01 11:00:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,8 +51,8 @@ submit.onclick=function() {
 request.open('GET','http://chsreenivas92.imad.hasura-app.io/submit-name?name='+user,true);
 request.send();
 };
-var login=document.getElementById("Login");
-var register=document.getElementById("Register");
+var login=$("#Login");
+var register=$("#Register");
 
 register.onclick=function(){
     console.log("clicked register button")
