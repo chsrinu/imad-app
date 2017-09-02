2ce1e5a7d333bc839ea1b32b26624156b7a781f1
chsrinu <chsreenivas92@gmail.com>
2017-09-02 11:47:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,7 +64,7 @@ $("#Register").click(function(){
         if(status==200)
             alert("logged in")
         else
-            console.log(JSON.parse(data)+" "+status)
+            console.log("returned data"+data+" "+status);
     });
 });
 
