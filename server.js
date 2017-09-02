1e41bbd5da4627c4683ffc13594cabaa077606ff
chsrinu <chsreenivas92@gmail.com>
2017-09-02 11:15:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.post('/register',function(req,res){
-    res.send("register endpoint successfull, the username and password sent are"+req.body.user1+" "+req.body.pass1);
+    res.send("register endpoint successfull, the username and password sent are"+req.body+" "+req.body.pass1);
 });
 
 app.post('/login',function(req,res){
