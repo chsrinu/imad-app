777511928167e9446ed8e9ba0269a83dadfed449
chsrinu <chsreenivas92@gmail.com>
2017-09-04 17:25:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -92,7 +92,7 @@ $("#Login").click(function(){
         complete: function() {
             //called when complete
             console.log('process complete');
-            window.location.replace("http://chsreenivas92.imad.hasura-app.io/articlelist")
+            window.location.replace("http://chsreenivas92.imad.hasura-app.io/articleone")
         },
         success : function(data)
         {
