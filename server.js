8d64309dc24eb846c04e1495a8ec965e44abfc7b
chsrinu <chsreenivas92@gmail.com>
2017-09-07 16:53:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.post('/login',function(req,res){
 app.post('/uploadcomment',function(req,res){
    var title=req.body.articleTitle
    var comment=req.body.commentText
-   pool.query("INSERT INTO articlecomments (articletitle, comments) VALUES ($1, $2)",[title,comment],function(results,err){
+   pool.query("INSERT INTO articlecomments(articletitle, comments) VALUES ($1,$2)",[title,comment],function(results,err){
        if(err)
         res.status(500).send(err.toString())
        /*else 
