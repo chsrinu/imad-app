1712294b6e6a7e2015ee959f3a14f9a1587db919
chsrinu <chsreenivas92@gmail.com>
2017-09-07 16:32:25 +0530

    [imad-console] Updates ui/articlescript.js

--- ui/articlescript.js
+++ ui/articlescript.js
@@ -19,19 +19,27 @@ if(submitbutton)
         console.log("submit button clicked")
         var commentText=$("#commentbox").val()
         inputparameters={articletitle:document.title,commentText:commentText}
-        if(len(commenttext)===0)
+        if(commentText.length === 0)
         alert("Please enter some text before submitting your comment")
         else//we need to update the comment in the database through a post request and reload the comments
         {
             $.ajax({
-                url:"http://chsreenivas92.imad.hasura-app.io/updatecomments",
+                url:"http://chsreenivas92.imad.hasura-app.io/uploadcomment",
                 type:'post',
                 contentType:'application/json',
                 data:JSON.stringify(inputparameters),
                 success : function(data){//reload the comments
+                    console.log("post request successful "+data.toString())
+                    var results=JSON.parse(data)
+                    var commentlist=""
+                    for(i=0;i<results.length;i++)
+                    {
+                        commentlist="<li>"+results[i]+"</li>"
+                    }
+                    $("#commentsList").html(commentlist);
                 },
                 error   : function(data){//show the error
-                    
+                     console.log(data.toString())
                 },
                 
         })
