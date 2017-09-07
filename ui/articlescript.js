bea354e8089402e35dde814d50ba5d8633f8c964
chsrinu <chsreenivas92@gmail.com>
2017-09-07 11:15:52 +0530

    [imad-console] Updates ui/articlescript.js

--- ui/articlescript.js
+++ ui/articlescript.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
     console.log("Jquery ready")
+    console.log("title is "+$("title").val());
 });
 
 if($("#commentbox"))
@@ -9,4 +10,25 @@ if($("#commentbox"))
 }
 else
     console.log("session ended")
-
+var submitbutton=$("#submit")
+if(submitbutton)
+{
+    submitbutton.onclick(function()
+    {
+        console.log("submit button clicked")
+        var commenttext=$("#commentbox").val()
+        if(len(commenttext)===0)
+        alert("Please enter some text before submitting your comment")
+        else//we need to update the comment in the database through a post request and reload the comments
+        {
+            $.ajax({
+                url:"http://chsreenivas92.imad.hasura-app.io/updatecomments",
+                type:'post',
+                contentType:'application/json',
+                data:JSON.stringify(inputparameters),
+                
+        })
+        }
+        
+    });
+}
