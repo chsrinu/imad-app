f2221cebbc8ed7cce29f27e9abfba118d462bceb
chsrinu <chsreenivas92@gmail.com>
2017-09-08 09:46:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/uploadcomment',function(req,res){
        if(err)
         res.status(500).send(err.toString())
        else
-        res.send("successfully updated the comments")
+        res.send("successfully updated the comments"+title+" and "+comment)
     });
 });
 app.get('/articlelist',function(req,res){
