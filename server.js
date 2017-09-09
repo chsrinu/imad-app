87887e91d2adf7ca1944768e2171405008b3d693
chsrinu <chsreenivas92@gmail.com>
2017-09-09 14:22:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,6 @@ app.get('/articles/:article_name',function(req,res){
 function createtemplate(data,req){
     var Atitle=data[0].title;
     var Acontent=data[0].content;
-    var sampletext="sample comment"
     var comments="";
     for(i=0;i<data.length;i++)
     {
